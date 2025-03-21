9c83bed2435cdd19a7ecc6f38fdb467a4ddce550
rdlopes17 <rauldlopes@gmail.com>
2025-03-21 08:33:41 -0300

    desenv pt 1 - implementando busca usando uma api externa.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent {
-
   listaLivros: [];
+  campoBusca = '';
 
-  constructor() { }
+  constructor(private service: LivroService) {}
 
+  buscarLivros() {
+    this.service.buscar(this.campoBusca);
+  }
 }
-
-
-
