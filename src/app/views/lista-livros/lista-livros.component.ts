25f080f28c26fdad1d9e698949726b4fec2cc665
rdlopes17 <rauldlopes@gmail.com>
2025-04-03 08:41:00 -0300

    desenv pt 5 - Conhecendo operadores do rxjs (map) e subscription

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,17 +7,22 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
   listaLivros: [];
   campoBusca = '';
+  subscription: Subscription;
 
   constructor(private service: LivroService) {}
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe({
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: (retornoApi) => console.log(),
       error: (error) => console.log(error),
       complete: () => console.log('Observable completado'),
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
